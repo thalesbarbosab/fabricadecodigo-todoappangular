96ac77218addb0e086cca7e1d1e7d06de2c9d672
Thales Bento <dev.tbarbosa.bento@gmail.com>
2021-05-18 08:09:30 -0300

    create a new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,21 @@ import { AppComponent } from './app.component';
 import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { TaskListItemComponent } from './tasks/task-list-item/task-list-item.component';
 import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { LoginComponent } from './account/login/login.component';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { HomeComponent } from './layout/home/home.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
     TaskListItemComponent,
-    TaskFormComponent
+    TaskFormComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    HomeComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
