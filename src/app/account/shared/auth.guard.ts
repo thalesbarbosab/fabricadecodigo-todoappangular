fac320036d111460809e9d037a56fd0474fffd50
Thales Bento <dev.tbarbosa.bento@gmail.com>
2021-05-18 08:30:25 -0300

    define auth guard and routes

--- src/app/account/shared/auth.guard.ts
+++ src/app/account/shared/auth.guard.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private router: Router){}
+
+  canActivate( next: ActivatedRouteSnapshot,
+               state: RouterStateSnapshot
+             ): boolean {
+    const token = window.localStorage.getItem('token');
+    if(token){
+      return true;
+    } else {
+      this.router.navigate(['login']);
+      return false;
+    }
   }
-  
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,44 @@
-import { TaskFormComponent } from './tasks/task-form/task-form.component';
-import { TaskListComponent } from './tasks/task-list/task-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { HomeComponent } from './layout/home/home.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
+import { TaskFormComponent } from './tasks/task-form/task-form.component';
+import { TaskListComponent } from './tasks/task-list/task-list.component';
+import { AuthGuard } from './account/shared/auth.guard';
+import { LoginComponent } from './account/login/login.component';
 
 const routes: Routes = [
-  { path: '', component: TaskListComponent },
-  { path: 'new', component: TaskFormComponent },
-  { path: 'edit/:id', component: TaskFormComponent }
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', component: TaskListComponent },
+      { path: 'new', component: TaskFormComponent },
+      { path: 'edit/:id', component: TaskFormComponent }
+    ],
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+        path: 'create-account',
+        component: CreateAccountComponent
+      }
+    ]
+  }
 ];
 
 @NgModule({
